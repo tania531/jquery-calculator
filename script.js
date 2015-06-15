9a2322a687f31860a6973ce77ba0467b21caf149
Tania <tania.dev77@gmail.com>
2015-06-15 09:23:18 -0700

    add last functions

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
 // Add your javascript here
 $(function(){
-  var num1, num2, num, op, calc;
+  var num1, num2, num, op, calc, percent;
   var calculation = [];
   var nums = [];
   var numbers = [];
   var num1 = [];
+  // var perc = [];
   $(".row").on("click", ".col-md-3", function(e){
     var val = $(this).html();
 
     console.log("$(val): ",val);
     if($(this).hasClass("operator")){
-      // calc = $($(this)[0]).text();
       calc = $($(this)[0]).text();
       calculation.push(calc);
     } else if ($(this).hasClass("num")){
       console.log("$(this).html(): ", $(this).html());
-      // nums.push(num);
-      // console.log("nums: ", nums);
-      //
-      // numbers = nums.join('');
-
       val = parseInt(val);
       calculation.push(val);
-
-
     } else if($(this).has("#calculate")){
       val = '';
-    }
-    // calculation.push(numbers);
+    } else if($(this).has("#decimal")){
+      calculation.push($(this).html());
+      $("#display").append(val);
+
+  } else if($(this).has("#sign")){
+    calculation.push($(this).html());
+    $("#display").append(val);
+  }
+
     $("#display").append(val);
   });
 
   $("#clear").click(function(){
     $("#display").html('');
   });
-  $("#percent").click(function(val){
-    $("#display").html("."+val);
-  });
 
   $("#calculate").on("click",  { value: calc },function(event){
-    console.log("event: ", event);
     console.log("calc: ", calc);
     doCalc(calc);
 
@@ -59,6 +55,13 @@ $(function(){
           case "/":
             divide();
             break;
+          case "%":
+            perc();
+            break;
+          case "+/-":
+            sign();
+            break;
+
 
           default:
             return;
@@ -117,11 +120,23 @@ $(function(){
     var num2 = calculation.splice(op + 1);
     num1 = parseInt(num1.join(''));
     num2 = parseInt(num2.join(''));
-    // calculation.splice(op,1);
-    // var total =  calculation.reduce(function(acc, c){
-    //   return acc / c;
-    // });
+
     total =  num1 / num2;
+    $("#display").html(total);
+  }
+
+  function perc(){
+    var op = calculation.indexOf("%");
+    var num1 = calculation.slice(0,op);
+    num1 = parseInt(num1.join(''));
+    total = num1 * 0.01;
+    $("#display").html(total);
+  }
+  function sign(){
+    var op = calculation.indexOf("+/-");
+    var num1 = calculation.slice(0,op);
+    num1 = parseInt(num1.join(''));
+    total = -num1;
     $("#display").html(total);
   }
 });
